81c9ad2f9a61a552ae7be3cd67b9b8a219999b04
ishankhan-866 <ish86690@gmail.com>
2026-03-18 23:39:29 +0530

    c2

--- main.tf
+++ main.tf
@@ -1,35 +1,33 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
 
-  name = "eks-vpc"
+ data "aws_vpc" "default" {
+  default = true
+}
 
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-north-1a", "eu-north-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
+
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
       instance_types = [var.node_instance_type]
-      min_size       = 1
+      min_size       = 2
       max_size       = 2
-      desired_size   = 1
+      desired_size   = 2
     }
   }
 }
