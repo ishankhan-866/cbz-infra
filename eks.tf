0376828a15b70e3a94cfee1238e51335d845f838
ishankhan-866 <ish86690@gmail.com>
2026-03-18 19:22:15 +0530

    up.tf

--- eks.tf
+++ eks.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# VPC
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
@@ -10,19 +9,26 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-north-1", "eu-north-1"]
+  azs             = ["eu-north-1a", "eu-north-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
   tags = {
     "kubernetes.io/cluster/eks-cluster" = "shared"
   }
 }
 
-# EKS Cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
@@ -30,21 +36,20 @@ module "eks" {
   cluster_name    = "eks-cluster"
   cluster_version = "1.29"
 
-  subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   enable_irsa = true
+  cluster_endpoint_public_access = true
 
-  # Node group (2 nodes)
   eks_managed_node_groups = {
     default = {
       desired_size = 2
       max_size     = 2
       min_size     = 2
 
-      instance_types = ["t3.micro"]
-
-      capacity_type = "ON_DEMAND"
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 
